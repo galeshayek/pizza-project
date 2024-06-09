f52f9c2c842faece0beaa5346d3b074bc5c79286
galeshayek <galeshayek15@gmail.com>
2024-06-09 21:22:25 +0300

    .

--- client/src/service/url.ts
+++ client/src/service/url.ts
@@ -1,2 +1,2 @@
-export const userUrl = `${process.env.URL}/api/v1/users` || 'http://localhost:8080/api/v1/users'
-export const recipeUrl = `${process.env.URL}/api/v1/recipes` || 'http://localhost:8080/api/v1/recipes'
+export const userUrl = `${process.env.URL}api/v1/users` || 'http://localhost:8080/api/v1/users'
+export const recipeUrl = `${process.env.URL}api/v1/recipes` || 'http://localhost:8080/api/v1/recipes'
